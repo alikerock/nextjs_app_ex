7f2864bd40d1b1e46da96632ed2ae564dc5a6882
rock's ezweb <alikerock@gmail.com>
2024-11-13 15:22:12 +0900

    글입력후 이동

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,8 @@
 "use client";
+import { useRouter } from 'next/navigation'
+
 export default function Create() {
+  const router = useRouter();
   const onSubmit = (e)=>{
     e.preventDefault();
     const title = e.target.title.value;
@@ -15,6 +18,8 @@ export default function Create() {
       .then(res=>res.json()) //결과를 객체로 변환
       .then(result=>{
         console.log(result);
+        router.push(`/read/${result.id}`);
+        router.refresh();
       })
   }
   return (
