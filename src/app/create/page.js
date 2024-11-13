50a4be80577ca4d01562880f49b91beecec4a801
rock's ezweb <alikerock@gmail.com>
2024-11-13 13:22:46 +0900

    클라이언트로 변경, 내용 입력 전송

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,34 @@
+"use client";
 export default function Create() {
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    const options = {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({title, body}) //object->json
+    }
+    fetch('http://localhost:9999/topics', options)
+      .then(res=>res.json()) //결과를 객체로 변환
+      .then(result=>{
+        console.log(result);
+      })
+  }
   return (
     <div>
-      <p>Create!</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="title" placeholder="title"/>
+        </div>
+        <div>
+          <textarea name="body" placeholder="content"></textarea>
+        </div>
+        <button type="submit">전송</button>
+      </form>
+      <hr/>
     </div>
   );
 }
