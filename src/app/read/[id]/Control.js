7cae01a4cf69e790044899c52f0280794c58fc75
rock's ezweb <alikerock@gmail.com>
2024-11-14 10:22:21 +0900

    deleteTopic 함수 생성

--- src/app/read/[id]/Control.js
+++ src/app/read/[id]/Control.js
@@ -5,12 +5,23 @@ import { useParams } from "next/navigation";
 const Control = ()=>{
   const params = useParams();
   const id = params.id;
+
+  const deleteTopic  = ()=>{
+    const option = {method:'DELETE'};
+    fetch('http://localhost:9999/topics'+id,option)
+    .then(res=>{
+       return res.json();//json->object
+    })
+    .then(result=>{     
+    });
+  }
+
   return(
     <ul>
       <li><Link href="/create">Create</Link></li>
       {id && <>
         <li><Link href={`/update/${id}`}>Update</Link></li>
-        <li><button>delete</button></li>
+        <li><button onClick={deleteTopic}>delete</button></li>
       </>}     
     </ul> 
   )
