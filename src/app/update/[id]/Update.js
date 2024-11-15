4dcf993c2ae4c2f7abb264bc4da11eec82b85b63
rock's ezweb <alikerock@gmail.com>
2024-11-15 16:35:02 +0900

    generateStaticParams 분리

---
+++ src/app/update/[id]/Update.js
@@ -0,0 +1,67 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function Update({ id }) {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}topics/${id}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setTitle(result.title);
+        setBody(result.body);
+      });
+  }, [id]);
+
+  const router = useRouter();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, body }),
+    };
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}topics/${id}`, options)
+      .then((res) => res.json())
+      .then((result) => {
+        router.push(`/read/${result.id}`);
+        router.refresh();
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            placeholder="title"
+          />
+        </div>
+        <div>
+          <textarea
+            name="body"
+            placeholder="content"
+            value={body}
+            onChange={(e) => {
+              setBody(e.target.value);
+            }}
+          ></textarea>
+        </div>
+        <button type="submit">전송</button>
+      </form>
+      <hr />
+    </div>
+  );
+}
