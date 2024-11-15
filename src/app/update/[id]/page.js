57e42fc4574dc1f9c7ca75294f812b343f4db8c1
rock's ezweb <alikerock@gmail.com>
2024-11-15 16:12:15 +0900

    update page params 함수 수정

--- src/app/update/[id]/page.js
+++ src/app/update/[id]/page.js
@@ -1,3 +1,12 @@
+export async function generateStaticParams() {
+  const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}topics`);
+  const topics = await resp.json();
+ 
+  return topics.map((topic) => ({
+    id: topic.id.toString(),
+  }));
+}
+
 "use client";
 import { useRouter,useParams } from 'next/navigation'
 import { useEffect, useState } from 'react';
